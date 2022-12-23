90196c6845549624ead3d669b88c636e6f5c0a8a
bo <bo_l_thompson@yahoo.com>
2022-12-23 12:05:06 -0800

    changed export syntax added decodeString corrected character endianness fixed typo in cider's name

--- src/services/mvTables.js
+++ src/services/mvTables.js
@@ -1,5 +1,6 @@
-module.exports = {
-  sectionmap: [
+import bf from "services/byteFuncs"
+
+export const sectionmap = [
     {
       start:0x0000,
       length:0x1000,
@@ -21,9 +22,9 @@ module.exports = {
       length:0x2000,
       alt:0xc000
     },
-  ],
+  ]
   
-  portraitMap: [
+export const portraitMap = [
     /*0x00*/"male",
     /*0x01*/"female",
     /*0x02*/"kirsche",
@@ -35,7 +36,7 @@ module.exports = {
     /*0x08*/"lemon",
     /*0x09*/"cabernet",
     /*0x0a*/"cassis",
-    /*0x0b*/"cidre",
+    /*0x0b*/"cider",
     /*0x0c*/"arancia",
     /*0x0d*/"cafeaulait",
     /*0x0e*/"pistachio",
@@ -83,17 +84,53 @@ module.exports = {
     /*0x38*/"rock triclops?",
     /*0x39*/"toasty",
     /*0x3a*/"flow",
-  ],
+  ]
   
-  charmap: {
+export const charmap = {
     0x0000:"❗",
-    0x1000:"★",
-    0x2000:" ",
-    0x3000:"X",
-    0x4000:"➩",
-    0x5000:"P",
-    0x6000:"ぁ",
-    0x7000:"あ",
-    0x8000:"け"
+    0x0010:"★",
+    0x0020:" ",
+    0x0030:"X",
+    0x0040:"➩",
+    0x0050:"P",
+    0x0060:"ぁ",
+    0x0070:"あ",
+    0x0080:"け",
+    0x043f:"豆",
+    0x04ae:"腐",
   }
+
+export function decodeString(buffer,addr,maxLength) {
+  let str = ""
+  for (let i = 0; i < maxLength; i++) {
+    const code = bf.endSwap(bf.read16(buffer,addr+i*2))
+
+    //stop at EOF control character
+    if (code === 0xFFFF)
+      break;
+    
+    str = str + (code in charmap ? charmap[code] : "?")
+  }
+  
+  return str
 }
+
+//78 - 7f party member count, location
+//70 - 77 contains bura, playtime
+//16x78 - location?
+//6a - amigo count? not very responsive though
+//6c - amigo 1 fire seirei level?
+//78 - amigo 1's assigned party member?
+
+//16x7a is party size (includes protag)
+//16x0e is protagonist's portrait
+//16x1a is party member 1's portrait
+  //spacing of c
+//16x26 is party member 2's portrait
+//16x32 is party member 3's portrait
+//16x3e is party member 4's portrait
+//16x4a is party member 5's portrait
+
+//area name is indexed
+//suspect that 8x1a is the number of registered amigos
+//suspect that 8x92 is amigo1's fire spirit level, each spirit following suit

d3303eedd9b6da80bd40e7a6a9a43e1708b82125
Bo Thompson <bo_l_thompson@yahoo.com>
2022-12-25 20:45:11 -0800

    basic view of protag's spirit levels

---
+++ src/scenes/StatsPane.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {seireiDefs} from 'services/mvTables'
+import bf from 'services/byteFuncs'
+import { useSave } from 'services/saveFile';
+
+const characterDefs = [
+  {
+    name:"Protagonist",
+    addr: 0x5130,
+  }
+]
+
+export default function StatsPane(props) {
+  const save = useSave();
+
+  return <tt>
+    <table>
+      <thead>
+        <tr>
+          <th>
+            Character
+          </th>
+          {
+            seireiDefs.map(s => 
+              <th
+                key={s.kanji}
+              >
+                {s.kanji}
+              </th>  
+            )
+          }
+        </tr>
+      </thead>
+      <tbody>
+        {
+          characterDefs.map(c => 
+            <tr
+              key={c.addr}
+            >
+              <td>
+                c.name
+              </td>
+              {
+                Object.entries(seireiDefs).map(s =>
+                  <th
+                    key={s[0]}
+                  >
+                    {bf.read16(save.data,(c.addr+0x50)+s[0]*2)}
+                  </th>
+                )
+              }
+            </tr>
+          )
+        }
+      </tbody>
+    </table>
+  </tt>
+}

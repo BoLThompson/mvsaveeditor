9787adc3a833ff69bcd6f13db26e03c320e695d3
Bo Thompson <bo_l_thompson@yahoo.com>
2022-12-25 21:53:38 -0800

    checksum and header now calculated immediately and saved added update() method

--- src/services/saveFile.js
+++ src/services/saveFile.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { sectionmap } from './mvTables';
+import bf from 'services/byteFuncs'
 
 const SaveContext = React.createContext();
 
@@ -14,33 +16,30 @@ function SaveProvider(props) {
     data:null
   });
 
+  function readFile(raw, filename) {
+    setData(oldData => ({
+      raw:raw,
+      headers: sectionmap.map(s =>
+        bf.header(raw,s.start)
+      ),
+      checksums: sectionmap.map(s =>
+        bf.checksum(raw,s.start,s.length)
+      ),
+      filename: filename ? filename : oldData.filename
+    }))
+  }
+
   const api = {
-
-    // updateData(cb) {
-    //   let dst = new ArrayBuffer(data.byteLength)
-    //   new Uint8Array(dst).set(new Uint8Array(data))
-
-    //   cb(data,dst)
-
-    //   setData(dst)
-    // },
-
     handleFileChange(e){
       //exit if there's no input file
       if (! e.target.files.length) 
         return false
       
       const file = e.target.files[0];
-
       const reader = new FileReader();
-  
+
       reader.onload = async ({target}) => {
-        console.log("got to the async")
-        console.log(target)
-        setData({
-          raw:target.result,
-          filename:file.name
-        })
+        readFile(target.result,file.name)
       }
   
       reader.readAsArrayBuffer(file)
@@ -59,12 +58,23 @@ function SaveProvider(props) {
       link.click();
       link.parentNode.removeChild(link);
     },
+
+    update(cb) {
+      let dst = new ArrayBuffer(data.raw.byteLength)
+      new Uint8Array(dst).set(new Uint8Array(data.raw))
+
+      const newRaw = cb(dst)
+
+      readFile(newRaw)
+    },
   }
 
   return <SaveContext.Provider
     value={{
       ...api,
       data:data.raw,
+      headers:data.headers,
+      checksums:data.checksums
     }}
   >
     {props.children}
